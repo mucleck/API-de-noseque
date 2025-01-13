673003de0b00458cc47c256987a2acf99d33e095
mucleck <franconogalesali@gmail.com>
2025-01-13 16:36:07 +0100

    Va no me jodas

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+//RUTA
+app.get("/", (req,res) =>{
+    res.send('Va no me jodas')
+})
+
+app.listen(port, () => console.log("Hola", port));
