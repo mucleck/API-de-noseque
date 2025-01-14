89d3a69c0e48b2e55be5481366f4d9c79728509c
mucleck <franconogalesali@gmail.com>
2025-01-14 15:30:27 +0100

    Pilotes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 9000;
@@ -11,3 +12,7 @@ app.get("/", (req,res) =>{
 
 app.listen(port, () => console.log("Hola", port));
 
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => console.log("Connected DB"))
+.catch((error) => console.error(error))
