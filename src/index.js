5af11c998fa30de95a6f77a331ae02d2f78679d1
mucleck <franconogalesali@gmail.com>
2025-01-27 11:32:41 +0100

    Venga va

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const rutasEspecie = require('./routes/especies.js');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 9000;
 
-
 // Conexión a MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Conexión exitosa a MongoDB'))
+mongoose.connect(process.env.MONGODB_URI)
+.then((response) => console.log("Todo bien"))
 .catch((error) => console.error('Error al conectar a MongoDB:', error));
 
 // Middleware
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use('/api', rutasEspecie);
 
 // Ruta de prueba
 app.get("/", (req, res) => {
